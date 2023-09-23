d469c10de04ae921604ed752abeaacede29b54f1
Denny Petersson <petersson.denny@gmail.com>
2023-09-23 10:05:39 +0200

    adding grayscale filter

--- ImageManipulator.js
+++ ImageManipulator.js
@@ -3,6 +3,9 @@ class ImageManipulator {
     this.canvas = canvas
     this.context = canvas.getContext('2d')
     this.image = new Image()
+  }
+
+  setImageData() {
     this.imageData = this.context.getImageData(
       0,
       0,
@@ -11,14 +14,49 @@ class ImageManipulator {
     )
   }
 
+  getRgbaData() {
+    return this.imageData.data
+  }
+
   drawImageIntoContext(imageSrc) {
     this.image.onload = () => {
       this.canvas.width = this.image.naturalWidth
       this.canvas.height = this.image.naturalHeight
       this.context.drawImage(this.image, 0, 0)
+      this.setImageData()
     }
 
     this.image.src = imageSrc
+  }
+
+  putManipulatedImageIntoContext() {
+    this.context.putImageData(this.imageData, 0, 0)
+  }
+
+  applyFilterGrayscale() {
+    const rgbaData = this.getRgbaData()
+
+    // source of formula: https://en.wikipedia.org/wiki/Grayscale#Luma_coding_in_video_systems
+    const redGrayscaleConstant = 0.299
+    const greenGrayscaleConstant = 0.587
+    const blueGrayscaleConstant = 0.114
+    const numberOfValuesInPixel = 4
+    for (let i = 0; i < rgbaData.length; i += numberOfValuesInPixel) {
+      let red = rgbaData[i]
+      let green = rgbaData[i + 1]
+      let blue = rgbaData[i + 2]
+
+      const grayscale =
+        red * redGrayscaleConstant +
+        green * greenGrayscaleConstant +
+        blue * blueGrayscaleConstant
+
+      rgbaData[i] = grayscale
+      rgbaData[i + 1] = grayscale
+      rgbaData[i + 2] = grayscale
+    }
+
+    this.putManipulatedImageIntoContext()
   }
 }
 
--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const canvas = document.getElementById('canvas')
 
 const imageManipulator = new ImageManipulator(canvas)
 imageManipulator.drawImageIntoContext('wepik-export-20230922142657alsS.jpeg')
+
+const grayscaleButton = document.getElementById('btn-grayscale')
+grayscaleButton.addEventListener('click', () => {
+  imageManipulator.applyFilterGrayscale()
+})
